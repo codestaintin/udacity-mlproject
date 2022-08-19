2b9281bcffbcacca375c90ce75cc1ac1275c23ac
Mohammed Isioye <mohzak1103@gmail.com>
2022-08-19 15:04:33 +0100

    Push project files

---
+++ run_kubernetes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath=mohzak1103/mlproject:v1.0
+
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run mlproject\
+ --image=$dockerpath\
+  --port=80\
+  --labels="app=mlproject"
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward mlproject 8000:80
+
